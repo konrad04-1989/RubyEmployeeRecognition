62b404e179031983eb7465531a91f498ffaac96f
Konrad Pawlowski <konrad.pawlowski@outlook.com>
2021-11-29 01:19:38 +0100

    Pierwsze zmiany

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Employee.create(email:"emp1@reca.com" , encrypted_password:"zaq12wsx")
+Employee.create(email:"emp2@reca.com" , encrypted_password:"zaq12wsx")
+Employee.create(email:"emp3@reca.com" , encrypted_password:"zaq12wsx")
+Employee.create(email:"emp4@reca.com" , encrypted_password:"zaq12wsx")
+Employee.create(email:"emp5@reca.com" , encrypted_password:"zaq12wsx")
