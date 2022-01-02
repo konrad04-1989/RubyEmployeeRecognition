e927c31cd88ef6aabc3d54cab3f9204471288126
Konrad Pawlowski <konrad.pawlowski@outlook.com>
2022-01-02 19:36:39 +0100

    Rubocop corrections and removal of unnecessary spec files

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -6,6 +6,6 @@ class Employee < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :given_kudos, class_name: 'Kudo', foreign_key: 'giver_id', inverse_of: :giver_of_kudo
-  has_many :received_kudos, class_name: 'Kudo', foreign_key: 'receiver_id', inverse_of: :receiver_of_kudo
+  has_many :given_kudos, class_name: 'Kudo', foreign_key: 'giver_id', inverse_of: :giver_of_kudo, dependent: :destroy
+  has_many :received_kudos, class_name: 'Kudo', foreign_key: 'receiver_id', inverse_of: :receiver_of_kudo, dependent: :destroy
 end
