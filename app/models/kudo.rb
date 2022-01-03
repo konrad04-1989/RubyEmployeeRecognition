b181dbc95046b972df852ada27c6ffa6cd9771c9
Konrad Pawlowski <konrad.pawlowski@outlook.com>
2022-01-03 22:11:44 +0100

    Removed unnecessary validations

--- app/models/kudo.rb
+++ app/models/kudo.rb
@@ -3,8 +3,6 @@
 class Kudo < ApplicationRecord
   validates :title, presence: true, length: { minimum: 2 }
   validates :content, presence: true, length: { minimum: 2 }
-  validates :giver_id, presence: true
-  validates :receiver_id, presence: true
 
   belongs_to :giver_of_kudo, class_name: 'Employee', foreign_key: 'giver_id', inverse_of: :given_kudos
   belongs_to :receiver_of_kudo, class_name: 'Employee', foreign_key: 'receiver_id', inverse_of: :received_kudos
