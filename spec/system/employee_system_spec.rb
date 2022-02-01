8989e9b4154444dc592966c258f7b8fdc991d900
Konrad Pawlowski <konrad.pawlowski@outlook.com>
2022-02-01 23:49:46 +0100

    Fix Rubocop

--- spec/system/employee_system_spec.rb
+++ spec/system/employee_system_spec.rb
@@ -1,32 +1,34 @@
-require "rails_helper"
+# frozen_string_literal: true
 
-RSpec.describe "Employee management", :type => :system do
+require 'rails_helper'
+
+RSpec.describe 'Employee management', type: :system do
   before do
     driven_by(:rack_test)
   end
 
-  it "enables me to sign up" do
+  it 'enables me to sign up' do
     visit root_path
     click_link 'Register'
-    fill_in 'employee[email]', :with => "test@test.pl"
-    fill_in 'employee[password]', :with => "zaq123"
-    fill_in 'Password confirmation', :with => "zaq123"
+    fill_in 'employee[email]', with: 'test@test.pl'
+    fill_in 'employee[password]', with: 'zaq123'
+    fill_in 'Password confirmation', with: 'zaq123'
     click_button 'Sign up'
 
-    expect(page).to have_text("Logged as: test@test.pl")
+    expect(page).to have_text('Logged as: test@test.pl')
   end
 
-  it "enables me to sign in" do
+  it 'enables me to sign in' do
     FactoryBot.create :employee,  email: 'test2@test.pl',
                                   password: 'zaq123',
                                   password_confirmation: 'zaq123'
-    
+
     visit root_path
     click_link 'Log in'
-    fill_in 'employee[email]', :with => "test2@test.pl"
-    fill_in 'employee[password]', :with => "zaq123"
+    fill_in 'employee[email]', with: 'test2@test.pl'
+    fill_in 'employee[password]', with: 'zaq123'
     click_button 'Log in'
 
-    expect(page).to have_text("Logged as: test2@test.pl")
+    expect(page).to have_text('Logged as: test2@test.pl')
   end
 end
