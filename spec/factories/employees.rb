3517da1706cfccca0217146b56f8029d01f946b3
Konrad Pawlowski <konrad.pawlowski@outlook.com>
2022-02-01 21:16:55 +0100

    Fix Rubocop

--- spec/factories/employees.rb
+++ spec/factories/employees.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
-    factory :employee do
-      email { Faker::Internet.email }
-      encrypted_password { Faker::Internet.password }
-    end
-  end 
+  factory :employee do
+    email { Faker::Internet.email }
+    encrypted_password { Faker::Internet.password }
+  end
+end
