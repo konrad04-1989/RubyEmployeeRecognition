c5ce5fad6957fed9807877f70a685fb3c8c13ba5
Konrad Pawlowski <konrad.pawlowski@outlook.com>
2022-02-02 01:22:23 +0100

    Minor changes to employee_system_spec.rb

--- spec/system/employee_system_spec.rb
+++ spec/system/employee_system_spec.rb
@@ -21,17 +21,17 @@ RSpec.describe 'Employee management', type: :system do
   end
 
   it 'enables me to sign in' do
-    FactoryBot.create :employee,  email: 'test2@test.pl',
+    FactoryBot.create :employee,  email: 'test5@test.pl',
                                   password: 'zaq123',
                                   password_confirmation: 'zaq123'
 
     visit root_path
     click_link 'Log in'
-    fill_in 'employee[email]', with: 'test2@test.pl'
+    fill_in 'employee[email]', with: 'test5@test.pl'
     fill_in 'employee[password]', with: 'zaq123'
     click_button 'Log in'
 
-    expect(page).to have_text('Logged as: test2@test.pl')
+    expect(page).to have_text('Logged as: test5@test.pl')
   end
 
   # rubocop:enable RSpec/ExampleLength
