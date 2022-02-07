ea2ae9c84be46ae695213ce2e31364067b048294
Konrad Pawlowski <konrad.pawlowski@outlook.com>
2022-02-07 23:42:05 +0100

    PR changes in kudo_system_spec.rb

--- spec/system/kudo_system_spec.rb
+++ spec/system/kudo_system_spec.rb
@@ -7,31 +7,29 @@ RSpec.describe 'Kudo management', type: :system do
     driven_by(:rack_test)
   end
 
+  let(:employee) { create(:employee) }
+  let(:new_kudo) { build(:kudo) }
+  let(:edited_kudo) { build(:kudo) }
   # rubocop:disable RSpec/ExampleLength
+  # rubocop:disable RSpec/MultipleExpectations
 
   it 'enables me to create, edit and destroy kudos' do
-    employee = create(:employee)
-    kudo = create(:kudo)
-
     visit root_path
     click_on 'Log in'
-    fill_in 'employee[email]', with: employee.email
-    fill_in 'employee[password]', with: employee.password
+    fill_in 'Email', with: employee.email
+    fill_in 'Password', with: employee.password
     click_button 'Log in'
 
-    visit '/kudos'
-    expect(page).to have_text('Show')
-
     visit '/kudos/new'
-    fill_in 'kudo[title]', with: 'Test Title of Kudo'
-    fill_in 'kudo[content]', with: 'Test Content '
+    fill_in 'kudo[title]', with: new_kudo.title
+    fill_in 'kudo[content]', with: new_kudo.content
     select employee.email
     click_button 'Create Kudo'
     expect(page).to have_text('Kudo was successfully created.')
 
     visit '/kudos'
     click_on 'Edit...'
-    fill_in 'kudo[title]', with: 'Edited Test Title of Kudo'
+    fill_in 'kudo[title]', with: edited_kudo.title
     click_button 'Update Kudo'
     expect(page).to have_text('Kudo was successfully updated.')
 
@@ -41,4 +39,5 @@ RSpec.describe 'Kudo management', type: :system do
   end
 
   # rubocop:enable RSpec/ExampleLength
+  # rubocop:enable RSpec/MultipleExpectations
 end
