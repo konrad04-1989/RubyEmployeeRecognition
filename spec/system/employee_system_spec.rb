6bdf4a34a27abb20944cd5eeea6244dd8954b44d
Konrad Pawlowski <konrad.pawlowski@outlook.com>
2022-02-07 23:12:04 +0100

    PR changes in employee_system_spec.rb

--- spec/system/employee_system_spec.rb
+++ spec/system/employee_system_spec.rb
@@ -7,25 +7,25 @@ RSpec.describe 'Employee management', type: :system do
     driven_by(:rack_test)
   end
 
-  let(:employee) { create(:employee) }
-
   # rubocop:disable RSpec/ExampleLength
 
   it 'enables me to sign up' do
+    employee = build(:employee)
     visit root_path
     click_link 'Register'
-    fill_in 'employee[email]', with: 'newtest@test.pl'
-    fill_in 'employee[password]', with: employee.password
+    fill_in 'Email', with: employee.email
+    fill_in 'Password', with: employee.password
     fill_in 'Password confirmation', with: employee.password
     click_button 'Sign up'
     expect(page).to have_text('Welcome! You have signed up successfully.')
   end
 
   it 'enables me to sign in' do
+    employee = create(:employee)
     visit root_path
     click_link 'Log in'
-    fill_in 'employee[email]', with: employee.email
-    fill_in 'employee[password]', with: employee.password
+    fill_in 'Email', with: employee.email
+    fill_in 'Password', with: employee.password
     click_button 'Log in'
     expect(page).to have_text('Signed in successfully.')
   end
