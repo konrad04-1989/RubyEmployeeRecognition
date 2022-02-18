3d328d7fe481ca86c9053e5023d60f93afd4cf86
Konrad Pawlowski <konrad.pawlowski@outlook.com>
2022-02-19 00:14:22 +0100

    Admin model created.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :admins, path: 'admins'
+  devise_for :employees, path: 'employees'
   root to: 'pages#home'
   resources :kudos
-  devise_for :employees
   get '/pages' => 'pages#home'
   get '/pages/home' => 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
