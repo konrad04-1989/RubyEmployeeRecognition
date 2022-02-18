73f3a833c9919240968f8b6f1f2e7cbb779dea6f
Konrad Pawlowski <konrad.pawlowski@outlook.com>
2022-02-19 00:37:43 +0100

    Updated seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 puts "Employees table seeding..."
 
 1.upto(5) do |i|
@@ -16,6 +9,14 @@ puts "Employees table seeding..."
 end
 
 puts "Employees table seeding done"
+
+puts "Admins table seeding..."
+    admin = Admin.new
+    admin.email = "admin@rera.com"
+    admin.password = 'zaq123'
+    admin.password_confirmation = 'zaq123'
+    admin.save!
+puts "Admins table seeding done"
 
 puts "Kudos table seeding..."
 
