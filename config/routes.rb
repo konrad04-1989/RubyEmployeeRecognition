7eafdf8393d2dc06427950241f9446b12cff5ccb
Konrad Pawlowski <konrad.pawlowski@outlook.com>
2022-02-21 20:09:58 +0100

    Added redirecting Admin to dashboard view at signing in.

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@
 Rails.application.routes.draw do
   devise_for :admins, path: 'admins'
   devise_for :employees, path: 'employees'
+
+  authenticated :admin do
+    root to: 'admin#dashboard', as: :admin_root
+  end
+
   root to: 'pages#home'
   resources :kudos
   get '/pages' => 'pages#home'
