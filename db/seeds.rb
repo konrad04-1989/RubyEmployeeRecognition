31a045ead685d0d51bb6aade998768605a986d40
Konrad Pawlowski <konrad.pawlowski@outlook.com>
2022-03-02 01:22:13 +0100

    Changes due to PR: Changes in seeds.rb.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,32 +1,15 @@
 puts "Employees table seeding..."
-
 1.upto(5) do |i|
-    employee = Employee.new
-    employee.email = "emp#{i}@rera.com"
-    employee.password = 'zaq123'
-    employee.password_confirmation = 'zaq123'
-    employee.save!
+    Employee.create!(email: "emp#{i}@rera.com", password: 'zaq123')
 end
-
 puts "Employees table seeding done"
 
 puts "Admins table seeding..."
-    admin = Admin.new
-    admin.email = "admin@rera.com"
-    admin.password = 'zaq123'
-    admin.password_confirmation = 'zaq123'
-    admin.save!
+    AdminUser.create!(email: "admin@rera.com", password: 'zaq123')
 puts "Admins table seeding done"
 
 puts "Kudos table seeding..."
-
 5.times do
-    kudo = Kudo.new
-    kudo.title = Faker::Superhero.name
-    kudo.content = Faker::TvShows::Friends.quote
-    kudo.giver_id = Faker::Number.between(from: 1, to: 5)
-    kudo.receiver_id = Faker::Number.between(from: 1, to: 5)
-    kudo.save!
+    Kudo.create!(title: Faker::Superhero.name, content: Faker::TvShows::Friends.quote, giver_id: Faker::Number.between(from: 1, to: 5), receiver_id: Faker::Number.between(from: 1, to: 5))
 end
-
 puts "Kudos table seeding done"
