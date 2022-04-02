25bd18b20aeae800a90d18c0d5c6ac72eaf47bdd
Konrad Pawlowski <konrad.pawlowski@outlook.com>
2022-04-02 15:14:36 +0200

    Updated seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,22 @@
-puts "Employees table seeding..."
+puts "Seeding Employees..."
 1.upto(5) do |i|
     Employee.create!(email: "emp#{i}@rera.com", password: 'zaq123', number_of_available_kudos: Faker::Number.between(from: 1, to: 10))
 end
-puts "Employees table seeding done"
+puts "Done."
 
-puts "Admins table seeding..."
+puts "Seeding Admins..."
     AdminUser.create!(email: "admin@rera.com", password: 'zaq123')
-puts "Admins table seeding done"
+puts "Done."
 
-puts "Kudos table seeding..."
+puts "Seeding Kudos..."
 5.times do
     Kudo.create!(title: Faker::Superhero.name, content: Faker::TvShows::Friends.quote, giver_id: Faker::Number.between(from: 1, to: 5), receiver_id: Faker::Number.between(from: 1, to: 5))
 end
-puts "Kudos table seeding done"
+puts "Done."
+
+puts "Seeding Company Values..."
+    CompanyValue.create!(title: 'Honesty')
+    CompanyValue.create!(title: 'Ownership')
+    CompanyValue.create!(title: 'Accountability')
+    CompanyValue.create!(title: 'Passion')
+puts "Done."
