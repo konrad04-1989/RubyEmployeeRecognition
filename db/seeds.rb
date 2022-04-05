6bf44c7871db19ff3301780847929cfe99eb1b7f
Konrad Pawlowski <konrad.pawlowski@outlook.com>
2022-04-05 17:28:56 +0200

    Updated seeds.rb.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,11 @@
 puts "Seeding Employees..."
-1.upto(5) do |i|
-    Employee.create!(email: "emp#{i}@rera.com", password: 'zaq123', number_of_available_kudos: Faker::Number.between(from: 1, to: 10))
+1.upto(10) do |i|
+    Employee.create!(email: "emp#{i}@rera.com", password: 'zaq123', number_of_available_kudos: rand(1..10))
 end
-puts "Done."
+puts "Done. Created #{Employee.count} Employees"
 
 puts "Seeding Admins..."
     AdminUser.create!(email: "admin@rera.com", password: 'zaq123')
-puts "Done."
-
-puts "Seeding Kudos..."
-5.times do
-    Kudo.create!(title: Faker::Superhero.name, content: Faker::TvShows::Friends.quote, giver_id: Faker::Number.between(from: 1, to: 5), receiver_id: Faker::Number.between(from: 1, to: 5))
-end
 puts "Done."
 
 puts "Seeding Company Values..."
@@ -19,4 +13,14 @@ puts "Seeding Company Values..."
     CompanyValue.create!(title: 'Ownership')
     CompanyValue.create!(title: 'Accountability')
     CompanyValue.create!(title: 'Passion')
-puts "Done."
+puts "Done. Created #{CompanyValue.count} Company Values"
+
+puts "Seeding Kudos..."
+10.times do
+    Kudo.create!(   title: Faker::Superhero.name, 
+                    content: Faker::TvShows::Friends.quote, 
+                    giver_id: rand(1..10), 
+                    receiver_id: rand(1..10), 
+                    company_value_id: rand(1..4))
+end
+puts "Done. Created #{Kudo.count} Kudos"
