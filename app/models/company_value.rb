ed2580a495c165fb695e146fa322b67ac9f7bc14
Konrad Pawlowski <konrad.pawlowski@outlook.com>
2022-04-05 17:27:00 +0200

    Created new associations between models.

--- app/models/company_value.rb
+++ app/models/company_value.rb
@@ -2,4 +2,6 @@
 
 class CompanyValue < ApplicationRecord
   validates :title, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 2 }
+
+  has_many :kudos, dependent: :destroy
 end
