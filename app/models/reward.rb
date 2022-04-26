75666a39f2b74a63bb478d3df65bb9662da2f799
Konrad Pawlowski <konrad.pawlowski@outlook.com>
2022-04-26 23:57:27 +0200

    Minor change in reward model.

--- app/models/reward.rb
+++ app/models/reward.rb
@@ -5,5 +5,5 @@ class Reward < ApplicationRecord
   validates :description, presence: true, length: { minimum: 2 }
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 1 }
   has_many :orders, dependent: :destroy
-  has_many :buyers, through: :orders, class_name: 'Employee'
+  has_many :employees, through: :orders
 end
