26e3c8f75340e2b0148b23e833c088eb60ed431c
Konrad Pawlowski <konrad.pawlowski@outlook.com>
2022-04-26 23:39:48 +0200

    Order controller created.

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+class OrdersController < ApplicationController
+  before_action :authenticate_employee!
+
+  # GET /orders
+  def index
+    @orders = Order.all.includes(%i[reward employee])
+  end
+
+  # POST /orders
+  def create
+    if current_employee.number_of_points >= reward.price
+      Order.create(employee: current_employee, reward: reward)
+      redirect_to orders_path, notice: 'Reward was successfully purchased.'
+    else
+      redirect_to rewards_path, alert: "Sorry, You don't have enough points."
+    end
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+
+  def reward
+    @reward = Reward.find(params[:reward_id])
+  end
+end
