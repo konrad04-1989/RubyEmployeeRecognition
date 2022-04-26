8e35a31aaa5f4ab4f7ab0e0355ffbc95f8ff1539
Konrad Pawlowski <konrad.pawlowski@outlook.com>
2022-04-26 23:38:44 +0200

    Related models updated.

--- app/models/reward.rb
+++ app/models/reward.rb
@@ -4,4 +4,6 @@ class Reward < ApplicationRecord
   validates :title, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 2 }
   validates :description, presence: true, length: { minimum: 2 }
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 1 }
+  has_many :orders, dependent: :destroy
+  has_many :buyers, through: :orders, class_name: 'Employee'
 end
