4626e903494bfc3c7f6b4cc60ee9eb907a6f483c
Konrad Pawlowski <konrad.pawlowski@outlook.com>
2022-04-26 23:34:58 +0200

    Created Order model.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_10_120438) do
+ActiveRecord::Schema.define(version: 2022_04_21_215208) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,6 +60,15 @@ ActiveRecord::Schema.define(version: 2022_04_10_120438) do
     t.index ["receiver_id"], name: "index_kudos_on_receiver_id"
   end
 
+  create_table "orders", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "employee_id", null: false
+    t.bigint "reward_id", null: false
+    t.index ["employee_id"], name: "index_orders_on_employee_id"
+    t.index ["reward_id"], name: "index_orders_on_reward_id"
+  end
+
   create_table "rewards", force: :cascade do |t|
     t.string "title", null: false
     t.string "description", null: false
@@ -70,4 +79,6 @@ ActiveRecord::Schema.define(version: 2022_04_10_120438) do
   end
 
   add_foreign_key "kudos", "company_values"
+  add_foreign_key "orders", "employees"
+  add_foreign_key "orders", "rewards"
 end
