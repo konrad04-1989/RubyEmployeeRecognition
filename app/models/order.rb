1375c3455395ad70583f6294440272d2f5d4f3da
Konrad Pawlowski <konrad.pawlowski@outlook.com>
2022-06-01 23:14:34 +0200

    Order model updated.

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,6 +2,7 @@
 
 class Order < ApplicationRecord
   serialize :snapshot
+  enum status: { not_delivered: 0, delivered: 1 }, _prefix: :status, _default: 'not_delivered'
 
   belongs_to :employee
   belongs_to :reward
