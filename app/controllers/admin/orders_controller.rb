46bcd5cbbe1602ab50c3bba35dea4142430237c1
Konrad Pawlowski <konrad.pawlowski@outlook.com>
2022-06-01 23:14:49 +0200

    Order controller updated.

---
+++ app/controllers/admin/orders_controller.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module Admin
+  class OrdersController < AdminController
+    def index
+      @orders = Order.all.includes([:employee]).order(:status)
+    end
+
+    def update
+      @order = Order.find(params[:id])
+      @order.status = 1
+      @order.save
+      redirect_to admin_orders_path, notice: 'Order delivered successfully.'
+    end
+  end
+end
